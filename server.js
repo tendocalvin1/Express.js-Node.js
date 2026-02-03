5459fcd4878bb5733f5e66300329869f718ab80c
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-02-03 22:41:42 +0300

    Add a server that I used to create different APIs using Node,js

---
+++ server.js
@@ -0,0 +1,15 @@
+// HTTP Module & create server.
+
+import http from 'http';
+const PORT = 8000;
+const server = http.createServer((req, res)=>{
+  
+    // res.setHeader('Content-type', 'text/html')
+    // res.statusCode = 404;
+    res.writeHead(500, {'content-type': 'application/json'})
+    res.end(JSON.stringify({message: "Internal Server Error"}))
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
