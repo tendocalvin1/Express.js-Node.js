7e8d5891b2f1e50083dc2a66ae24fec1cd6e5229
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-02-03 21:54:16 +0300

    ES Module use for making and testing some APIs like how we shall create APIs using Express.js

---
+++ PostController.js
@@ -0,0 +1,11 @@
+const posts = [
+    {id: 1, title:"post one"},
+    {id: 2, title:"post two"},
+    {id: 3, title:"post three"}
+]
+
+export const getPosts =()=> posts
+export const getPostsLength = () => posts.length
+
+export default getPosts;
+
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
-const {generateRandomNumber, celciusToFahreinheit} = require('./utils')
+// const {generateRandomNumber, celciusToFahreinheit} = require('./utils')
 
-console.log(`Random Number: ${generateRandomNumber()}`)
-console.log(`Celcius to fahreinheit: ${celciusToFahreinheit(0)}`)
+// console.log(`Random Number: ${generateRandomNumber()}`)
+// console.log(`Celcius to fahreinheit: ${celciusToFahreinheit(0)}`)
+
+import getPosts, {getPostsLength} from "./PostController.js";
+console.log(getPosts());
+console.log(`posts length: ${getPostsLength()}`)
