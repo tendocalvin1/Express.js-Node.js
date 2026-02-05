0f5e1ffeef1307e742a1c8d43047b5e18f72e747
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-02-05 19:27:08 +0300

    Geeting the number of posts using an HTTP method called GET

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 // HTTP Module & create server.
 
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 const server = http.createServer((req, res)=>{
-  
-    // res.setHeader('Content-type', 'text/html')
-    // res.statusCode = 404;
-    res.writeHead(500, {'content-type': 'application/json'})
-    res.end(JSON.stringify({message: "Internal Server Error"}))
+    try{
+        // check if GET request
+        if(req.method === 'GET'){
+    if(req.url === '/'){
+res.writeHead(200, {'content-type': 'text/html'})
+// res.end(JSON.stringify({message: "Internal Server Error"}))
+res.end('<h1>Home Page</h1>')
+    }else if(req.url === '/about'){
+res.writeHead(200, {'content-type': 'text/html'})
+res.end('<h1>About Page</h1>')
+}else{
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.end('<h1>Not Found</h1>')
+}
+
+
+        }else{
+            throw new Error('Method not allowed')
+        }
+    }catch(error){
+    res.writeHead(500, {'content-type': 'text/html'})
+    res.end('<h1>Internal Server Error</h1>')
+    }
+
+   if(req.url === '/'){
+res.writeHead(200, {'content-type': 'text/html'})
+// res.end(JSON.stringify({message: "Internal Server Error"}))
+res.end('<h1>Home Page</h1>')
+    }else if(req.url === '/about'){
+res.writeHead(200, {'content-type': 'text/html'})
+res.end('<h1>About Page</h1>')
+}else{
+    res.writeHead(404, {'content-type': 'text/html'})
+    res.end('<h1>Not Found</h1>')
+}
+
+
 })
 
 server.listen(PORT, ()=>{
