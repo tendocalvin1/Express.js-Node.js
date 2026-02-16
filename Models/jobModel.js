4ed4a1ac4099add9775e0f958649b5e5d01bbe39
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-02-16 12:28:45 +0300

    Refine the MVC structure of the simple API for the job Portal I had created

---
+++ Models/jobModel.js
@@ -0,0 +1,8 @@
+
+
+export const jobs = [
+    {id:1, name: 'Tendo Calvin', title: 'Software Engineer'},
+    {id:2, name: 'David Beckham', title: 'Footballer'},
+    {id:3, name: 'Ruben Amorim', title: 'Football Manager'},
+    {id:4, name: 'Mark Goldbridge', title: 'Content Creator'}
+]
